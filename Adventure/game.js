83116ec81b47b80c0a31747e216cd3308494c1ad
Gavin <20gfry@gmail.com>
2019-01-22 10:30:12 -0700

    added stuff

---
+++ Adventure/game.js
@@ -0,0 +1,13 @@
+//writes a thing to a thing
+document.write("<h1>YOU HAVE A FACE! got em<h1/>");
+//kissing is just sucking someone else's face
+//var yo="mamma";
+//alert("yo " + yo)
+
+name = prompt("what be your callin' tag mate?")
+confirm("so your name be: " + name + "?")
+alert("strange name. yee mustn't be from 'round these parts... in any case, WELCOME TO MEE CREW!")
+
+//alert("WOAH, YOU GOTA GET YOUSELF OUT A HERE");
+//confirm("WHERE DO YOU WANT TO GO???????/???/???//??/??/?");
+//prompt("WHERE DO YOU WANT TO GO??? THAT WASN'T A REAL ANSWER");
