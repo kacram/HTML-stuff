96aa7b063a837477b4b19317b7a59faf3b685ac1
Gavin <20gfry@gmail.com>
2019-02-15 10:30:50 -0700

    added the ability to read things (again)

--- Adventure/game.js
+++ Adventure/game.js
@@ -75,11 +75,19 @@
     var eRiver = null;
 } //room enemies
 {
-    var House = [rHouse, iHouse, eHouse, dHouse];
-    var Basement = [rBasement, iBasement, eBasement, dBasement];
-    var Clearing = [rClearing, iClearing, eClearing, dClearing];
-    var River = [rRiver, iRiver, eRiver, dRiver];
+    //what the game tells you when you look in a room
+    var lHouse = "you find yourself in a small cottage. you have been living here for the past 2 months while working for a lumber-jack in the area near here. it is a one room house with little fronananture.";
+    var lBasement = "this is your basement. you keep several pickeled goods on the shelves just in case of emergency. in the corner there are several rusty swords that you used to use for practicing swordplay.";
+    var lClearing = "you are in a small clearing with a densely forested area on most sides. your cottage lies to the north of here while a small spring lies to the south. there is a mountain off to the distance to the east and a village to the west.";
+    var lRiver = "there is a little brook trickeling between the trees that caries down a small slope. the stream's origin is towards the east and it carries on towards the south. there is nothing but dense forest to the west. the clearing is to the north.";
+}//room look scripts
+{
+    var House = [rHouse, iHouse, eHouse, dHouse, lHouse];
+    var Basement = [rBasement, iBasement, eBasement, dBasement, lBasement];
+    var Clearing = [rClearing, iClearing, eClearing, dClearing, lClearing];
+    var River = [rRiver, iRiver, eRiver, dRiver, lRiver];
 } //room arrays
+//where the equipment is stored
 var equipment = {
     helm: null,
     chestPiece: null,
@@ -88,6 +96,7 @@ var equipment = {
     boots: null,
     weapon: null,
 }
+//where the game tracks your stats
 var stats = {
     vitality: 10,
     strength: 3,
@@ -96,20 +105,20 @@ var stats = {
     fortitude: 2,
     intellegence: 3,
 }
-
+//declares an aray where one can find information about the rooms
 var rooms = [House, Basement, Clearing, River];
-
+//sets your starting room
 var room = rHouse;
+//declares the player inventory
 var inventory = [cap, jacket, null, null, null, null];
-
-var jimathy = false;
-
+//get the player's name
 var name = prompt("What! is your name?");
 //begin the game
 Game();
 
 //the main game shell
 function Game() {
+    //this loop prevents the game from ending accidentally
     while (1 == 1) {
         //begin the room
         Room();
@@ -118,45 +127,26 @@ function Game() {
 //check which room to use and figure out how to do it
 function Room() {
     //a temporary inventory variable that lists items in a room
-    if (rooms[room][2] != null){
-        Combat();
-    }
-    
     var tempInv = " ";
     for(i = 0; i < 6; i++){
         if(rooms[room][1][i] != null){
             if(tempInv == " "){
-                tempInv += rooms[room][1][i];
+                tempInv += rooms[room][1][i].name;
             }
             else{
                 tempInv += " and a ";
-                tempInv += rooms[room][1][i];
+                tempInv += rooms[room][1][i].name;
             }
         }
     }
-    if (room == rHouse) {
-        if (jimathy == false){
-            alert("you find yourself in a small room filled with cobwebs. a musty smell permiates the air. there is a corpse sitting in the corner, its eyes sunken in and its dry cold skin wrapped tightly around it. there is a trap door laying on the floor underneath a disheveled carpet that has been obviously kicked aside. there is also a door to the south of you leading into a small clearing outside. on the floor there is a " + tempInv);
-        }
-        else{
-            alert("you find yourself in your holiday cabin. you have been here for years. times have been tough on you and your friend. Jimathy is laying dead in the corner. he has been there since the week after you got there. on the floor there ia a " + tempInv)
-        }
-        PlayerMove();
-    }
-    else if (room == rBasement) {
-        alert("you have entered a small cramped cellar full of kegs of what is presumably pickeled foods. there is a sword laying in the corner coated in rust and rediculusly dull. there is a ladder upwards and a hole in the corner that seems too small to crawl through. in the kegs there is a " + tempInv + ".");
-        PlayerMove();
-    }
-    else if (room == rClearing) {
-        alert("you find yourself outside the house in a clearing with trees surrounding the edge. the light above you is bright and beaming. there is a small creek running down the edge of the clearing and there are mountains through the trees to the north.there is a" + tempInv + " laying in the grass.");
-        PlayerMove();
-    }
-    else if (room == rRiver) {
-        alert("you find yourself standing next to a small stream. the cool water is trickeling lightly over some mossy stones imbedded in the river bed. there is a" + tempInv + ".")
+    if (tempInv == " "){
+        alert(rooms[room][4]);
     }
     else{
-        alert("you are floating in an infinite black obys, have fun!")
+        tempInv += ".";
+        alert(rooms[room][4] + " in the room there is a " + tempInv); 
     }
+    PlayerMove();
 }
 //take the player's input and figure out what to do
 function PlayerMove() {
@@ -164,10 +154,20 @@ function PlayerMove() {
         var reply = prompt("what do you do?").toLowerCase();
         //if you steal stuff
         if (reply == "take") {
-            let reply = prompt("what do you take?").toLowerCase();
+            reply = prompt("what do you take?").toLowerCase();
             var i = 0;
             var k = 0;
-            for (i=0; i < 6; i++) {
+            //for practice rusty swords
+            if (reply == "rusty sword" && room == rBasement){
+                for (i=0; i < inventory.length; i++){
+                    if (inventory[i] == null){
+                        alert ("you took the rusty sword");
+                        inventory[i] = rustySword;
+                        break;
+                    }
+                }
+            }
+            else for (i=0; i < 6; i++) {
                 k = i;
                 if (rooms[room][1][i] != null) {
                     if (rooms[room][1][i].name == reply)
@@ -190,7 +190,7 @@ function PlayerMove() {
         }
         //if the player drops an item
         else if (reply == "drop" || reply == "throw"){
-            let reply = prompt("What do you want to drop?");
+            reply = prompt("What do you want to drop?");
             var i = 0;
             var k = 0;
             for (i=0; i < 6; i++) {
@@ -228,7 +228,7 @@ function PlayerMove() {
         }
         //if you look around
         else if (reply == "look" || reply == "l") {
-            Room();
+            alert(rooms[room][4] + " in the room there is a " + tempInv);
         }
         //if you go north
         else if (reply == "n" || reply == "north") {
@@ -282,7 +282,6 @@ function PlayerMove() {
         }
         //if you go down
         else if (reply == "down" || reply == "d") {
-            alert(rooms[room][3][5])
             if (rooms[room][3][5] != null){
                 room = rooms[room][3][5];
                 Room();
@@ -294,43 +293,13 @@ function PlayerMove() {
         //if you read something
         else if (reply == "read") {
             reply = prompt("what would you like to read?")
-            if (reply == "letter"){
-                for(i = 0; i < 6; i++){
-                    if(inventory[i] == "letter"){
-                        alert('the letter reads as follows. "I hope you are having a great time out in the woods with your best friend jimathy von bricklestein! Your sister keeps asking me when you are going to get back. The house is so empty without your cheery presence. I hope you return safely from your hunting trip." the date at the bottom says 1/11/13. the current date is 1/30/19.')
-                        jimathy = true;
-                        for(i = 6; i < 6; i++) {
-                            if (inventory[i] == "corpse") {
-                                inventory[i] = "jimathy";
-                            }
-                            if (rooms[rHouse][1][i] == "corpse") {
-                                rooms[rHouse][1][i] = "jimathy";
-                            }
-                        }
-                        break;
-                    }
-                    else if(i == 6){
-                        alert("you don't have that.")
-                    }
+            for (i = 0; i < inventory.length; i++){
+                if (inventory[i].name == reply){
+                    alert(inventory[i].note);
                 }
-            }
-            else{
-                alert("you can't read that!")
-            }
-        }
-        //if you eat something
-        else if(reply == "eat") {
-            reply = prompt("what do you satisfy your hunger with?")
-            if (reply == "corpse" || reply == "jimathy"){
-                for(i=0; i < 6; i++){
-                    if (inventory[i] == "corpse" || inventory[i] == "jimathy"){
-                        alert("you take a bite of its worm eaten skin. it is cold and slimy skin makes you gag as it slides slowly down your throught. you take bite after bite, each one making you more sick than the last.")
-                        inventory[i] = null;
-                    }
-                }
-            }
-            else{
-                alert ("you can't bring yourself that low.")
+                else {
+                    alert("you can't read that!");
+                }   
             }
         }
         //if you want to exit
@@ -340,46 +309,47 @@ function PlayerMove() {
                 alert("by the way the goal was not to die...")
                 alert("you killed yourself")
             }
-            yeet;
+            page.close
         }
         //if you want to equip something
         else if(reply == "equip") {
-            let reply = prompt("what do you want to equip?");
+            var yeet = null;
+            reply = prompt("what do you want to equip?");
             for (i = 0; i <= inventory.length; i++){
                 if (inventory[i] != null){
                     if (inventory[i].name = prompt){
                         if (inventory[i].type == "helm"){
-                            let yeet = inventory[i];
+                            yeet = inventory[i];
                             inventory[i] = equipment.helm;
                             equipment.helm = yeet;
                             alert("You equiped the " + yeet.name + ".");
                         }
                         else if (inventory[i].type == "chestPlate") {
-                            let yeet = inventory[i];
+                            yeet = inventory[i];
                             inventory[i] = equipment.chestPiece;
                             equipment.chestPiece = yeet;
                             alert("You equiped the " + yeet.name + ".");
                         }
                         else if (inventory[i].type == "gauntlet") {
-                            let yeet = inventory[i];
+                            yeet = inventory[i];
                             inventory[i] = equipment.gauntlets;
                             equipment.helm = yeet;
                             alert("You equiped the " + yeet.name + ".");
                         }
                         else if (inventory[i].type == "leggings") {
-                            let yeet = inventory[i];
+                            yeet = inventory[i];
                             inventory[i] = equipment.leggings;
                             equipment.helm = yeet;
                             alert("You equiped the " + yeet.name + ".");
                         }
                         else if (inventory[i].type == "boots") {
-                            let yeet = inventory[i];
+                            yeet = inventory[i];
                             inventory[i] = equipment.boots;
                             equipment.helm = yeet;
                             alert("You equiped the " + yeet.name + ".");
                         }
                         else if (inventory[i].type == "weapon") {
-                            let yeet = inventory[i];
+                            yeet = inventory[i];
                             inventory[i] = equipment.weapon;
                             equipment.helm = yeet;
                             alert("You equiped the " + yeet.name + ".");
@@ -400,7 +370,7 @@ function PlayerMove() {
 //enemy encounters and the following combat
 function Combat() {
     var enemy = rooms[room][2];
-    alert()
+    alert("there is a " + enemy.name);
 }
 
 
